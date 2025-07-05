5e1ceebe4c7108893a046fbbf0dfba9c3758af50
mahmoudAliaboElhassan <ml6893254@gmail.com>
2025-07-06 02:56:48 +0300

    fixing pug in contacts

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,7 @@ import Skills from "./components/Skills/";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import { ToastContainer, toast } from "react-toastify";
-import Contacts from "./components/Contacts";
+import Contacts from "./components/contacts";
 
 const App = () => {
   return (
